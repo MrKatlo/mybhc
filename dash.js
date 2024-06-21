5c4b613bf3113400165962c4044940256fa31e6d
unknown <CTO@ITO01WX2100.gob.gov.bw>
2024-06-21 17:02:35 +0200

    tab added(not styled,panel for menu added)

--- dash.js
+++ dash.js
@@ -1,88 +1,122 @@
 import React, { useState, useEffect } from 'react';
-import { View,Image, Text, TextInput, Button, StyleSheet, ScrollView, Touchable, TouchableOpacity } from 'react-native';
+import { View, Image, Text, TextInput, Button, StyleSheet, ScrollView, Touchable, TouchableOpacity } from 'react-native';
 import { initializeApp } from '@firebase/app';
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from '@firebase/auth';
 import style from './backone'
 const firebaseConfig = {
-  apiKey: "AIzaSyDjNw56wdME-m08v-bxMf3NODd4gcAvr9U",
-  authDomain: "bwbhc-939aa.firebaseapp.com",
-  projectId: "bwbhc-939aa",
-  storageBucket: "bwbhc-939aa.appspot.com",
-  messagingSenderId: "408379258697",
-  appId: "1:408379258697:web:d01784891cfc39ea4e0c98",
-  measurementId: "G-H0W0EL6Z8N"
+    apiKey: "AIzaSyDjNw56wdME-m08v-bxMf3NODd4gcAvr9U",
+    authDomain: "bwbhc-939aa.firebaseapp.com",
+    projectId: "bwbhc-939aa",
+    storageBucket: "bwbhc-939aa.appspot.com",
+    messagingSenderId: "408379258697",
+    appId: "1:408379258697:web:d01784891cfc39ea4e0c98",
+    measurementId: "G-H0W0EL6Z8N"
 };
 
-export default function DashBoard(){
-return(
-    <View style={Stylers.dashcont}>
-        <View style={Stylers.forlogo}>
-            <Image style={Stylers.logo} source={require('./assets/logo.png')}/>
+export default function DashBoard() {
+    return (
+        <View style={Stylers.dashcont}>
+            <View style={Stylers.forlogo}>
+                <Image style={Stylers.logo} source={require('./assets/logo.png')} />
+            </View>
+
+            <Image style={Stylers.forslash} source={require('./assets/plash.png')} />
+
+            <View style={Stylers.searchbar}>
+                <Image style={Stylers.searchicon} source={require('./assets/search.png')} />
+                <TextInput style={Stylers.searchinput} placeholder='Search' ></TextInput>
+            </View>
+            <View style={Stylers.formenu}>
+                      <Text>
+                        menu here
+                      </Text>
+            </View>
+            { <View style={Stylers.tabs}>
+                <View>
+                    <Image style={Stylers.tablogo} source={require('./assets/home.png')} />
+                </View>
+                <View>
+                    <Image style={Stylers.tablogo} source={require('./assets/chat.png')} />
+                </View>
+                <View>
+                    <Image style={Stylers.tablogo} source={require('./assets/settings.png')} />
+                </View>
+                <View>
+                    <Image style={Stylers.tablogo} source={require('./assets/person.png')} />
+                </View>
+            </View>}
         </View>
-      
-        <Image style={Stylers.forslash} source={require('./assets/plash.png')}/>
-     
-        <View style={Stylers.searchbar}>
-        <Image style={Stylers.searchicon} source={require('./assets/search.png')}/>
-        <TextInput style={Stylers.searchinput} placeholder='Search' ></TextInput>
-        </View>
-        <View style={Stylers.tabs}> 
- 
-        </View>
-    </View>
-)
+    )
 }
 
 const Stylers = StyleSheet.create({
-    tabs:{
-     flexDirection:'row',
+    tabs: {
+        flex: 1,
+        justifyContent: 'space-between', // Align content to the bottom
+        alignItems: 'center', // Center horizontally if needed
+        flexDirection:"row",
+        width:"90%",
+        alignSelf:"center"
     },
-logo:{
-   alignSelf: 'flex-start', 
-   width:50,
-   height:40
-},
-forsplash:{
-alignSelf:'flex-start',
-width:"100%",
-height:'40%',
-backgroundColor:"black",
-
-
-},
-searchicon:{
-verticalAlign:'center',
-width:40,
-height:'50%',
-marginTop:19,
-marginLeft:10,
-},
-searchinput:{
-fontSize:18,
-color:"#AD2524"
-},
-searchbar:{
-width:'95%',
-height:65,
-borderWidth:2,
-borderColor:'#AD2524',
-marginTop:20,
-alignSelf:'center',
-borderRadius:27,
-flexDirection: "row",
-gap:5
-},
-forlogo:{
-
-},
-lash:{
-    alignSelf:'center',
+    tablogo:{
+        width: 50,
+        height: 40,
+        padding:10
+    },
+    formenu:{
     
-  
-   },
-dashcont:{
-    flexGrow: 1,
-    width:'100%',
-    backgroundColor: '#fff',
-}
+height:'38%',
+width:'100%',
+backgroundColor:"red"
+    },
+    logo: {
+        alignSelf: 'flex-start',
+        width: 50,
+        height: 40,
+      
+    },
+    forsplash: {
+        alignSelf: 'flex-start',
+        width: "100%",
+        height: '40%',
+        backgroundColor: "black",
+
+
+    },
+    searchicon: {
+        verticalAlign: 'center',
+        width: 40,
+        height: '50%',
+        marginTop: 19,
+        marginLeft: 10,
+    },
+    searchinput: {
+        fontSize: 18,
+        color: "#AD2524"
+    },
+    searchbar: {
+        width: '95%',
+        height: 65,
+        borderWidth: 2,
+        borderColor: '#AD2524',
+        marginTop: 20,
+        alignSelf: 'center',
+        borderRadius: 27,
+        flexDirection: "row",
+        gap: 5,
+        paddingBottom:15,
+    },
+    forlogo: {
+
+    },
+    lash: {
+        alignSelf: 'center',
+
+
+    },
+    dashcont: {
+        flexGrow: 1,
+        width: '100%',
+        backgroundColor: '#fff',
+    }
 });
