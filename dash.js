d6b6afa7f94e62317d2c54da30df51dcf3b15972
Bradley-makuke <136990865+Bradley-makuke@users.noreply.github.com>
2024-06-26 14:47:31 +0200

    Update dash.js

--- dash.js
+++ dash.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
 import { View, Image, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import DefaultUi from './searchbar';
@@ -17,37 +16,10 @@ export default function DashBoard() {
   // Menu item component
   const MenuItem = ({ iconSource, label }) => (
     <TouchableOpacity style={Stylers.menuItem} onPress={() => handleMenuItemPress(label)}>
-=======
-import React from 'react';
-import { View, Image, Text, TextInput, StyleSheet, TouchableOpacity, SafeAreaView } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-import {NavigationContainer, useNavigation } from "@react-navigation/native";
-import {createStackNavigator} from "@react-navigation/stack";
-import ReportsScreen from "./ReportsScreen";
-import HousesScreen from "./HousesScreen";
-import PaymentsScreen from "./PaymentsScreen";
-import StatementsScreen from "./StatementsScreen";
-import EnquiriesScreen from "./EnquiriesScreen";
-import Maincont.js from "./NAVIGATION/Maincont.js";
-
-const Stack = createStackNavigator();
-
-// Component for each menu item
-const MenuItem = ({ iconSource, label, routeName }) => {
-  const navigation = useNavigation();
-
-  const handlePress = () => {
-    navigation.navigate(routeName);
-  };
-
-  return (
-    <TouchableOpacity style={Stylers.menuItem} onPress={handlePress}>
->>>>>>> 95331b237b4181265ce3123a9d8bf83cde61ffe0
       <Image source={iconSource} style={Stylers.icon} />
       <Text>{label}</Text>
     </TouchableOpacity>
   );
-<<<<<<< HEAD
 const Menu=(
   <View style={Stylers.menuContainer}>
   <MenuItem iconSource={require('./assets/rent.png')} label="View Houses" />
@@ -97,24 +69,6 @@ const Menu=(
         console.log("Unknown action");
         return DefaultView; // Or you can return null if you prefer
     }
-=======
-};
-
-export default function DashBoard() {
- 
-
-  const Menu = () => {
-    return (
-      <View style={Stylers.menuContainer}>
-        <MenuItem iconSource={require('./assets/rent.png')} label="View Houses" routeName= "Houses" />
-        <MenuItem iconSource={require('./assets/pay.png')} label="Make Payments" routeName= "Payments"  />
-        <MenuItem iconSource={require('./assets/statement.png')} label="View Statements" routeName= "Statements" />
-        <MenuItem iconSource={require('./assets/report.png')} label="Reports" routeName= "Reports"  />
-        <MenuItem iconSource={require('./assets/info.png')} label="General Enquiries"  routeName= "Enquiries"/>
-        <MenuItem iconSource={require('./assets/add.png')} label=""  />
-      </View>
-    );
->>>>>>> 95331b237b4181265ce3123a9d8bf83cde61ffe0
   };
 
   // Render component
@@ -123,96 +77,15 @@ export default function DashBoard() {
       <View style={Stylers.container}>
         {getCurrentView()}
       </View>
-<<<<<<< HEAD
    
       
     </View>
-=======
-
-      <Image style={Stylers.forsplash} source={require('./assets/plash.png')} />
-
-      <View style={Stylers.searchbar}>
-        <Image style={Stylers.searchicon} source={require('./assets/search.png')} />
-        <TextInput style={Stylers.searchinput} placeholder='Search' />
-      </View>
-<      NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Menu" component={Menu} />
-        <Stack.Screen name="Houses" component={HousesScreen} />
-        <Stack.Screen name="Reports" component={ReportsScreen} />
-        <Stack.Screen name="Enquiries" component={EnquiriesScreen} />
-        <Stack.Screen name="Payments" component={PaymentsScreen} />
-        <Stack.Screen name="Statements" component={StatementsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-    <Maincont/>
-    </SafeAreaView>
->>>>>>> 95331b237b4181265ce3123a9d8bf83cde61ffe0
   );
 }
 
 const Stylers = StyleSheet.create({
-<<<<<<< HEAD
   container: {
     flex: 1,
-=======
-  /*btn: {
-    width: '' // Update as necessary
-  },
-  tablogo: {
-    width: 40,
-    height: 30,
-  },
-  formenu: {
-    flex: 1,
-    height: '36%',
-    width: '100%',
-    flexDirection: "row",
-    flexWrap: "wrap",
-    padding: 10,
-  },*/
-  logo: {
-    alignSelf: 'flex-start',
-    width: 55,
-    height: 40,
-  },
-  forsplash: {
-    alignSelf: 'flex-start',
-    width: "100%",
-    height: '40%',
-    backgroundColor: "black",
-  },
-  searchicon: {
-    marginLeft: 10,
-    width: 40,
-    height: '50%',
-    marginTop: 19,
-  },
-  searchinput: {
-    fontSize: 18,
-    color: "#AD2524"
-  },
-  searchbar: {
-    width: '95%',
-    height: 60,
-    borderWidth: 2,
-    borderColor: '#AD2524',
-    marginTop: 20,
-    alignSelf: 'center',
-    borderRadius: 27,
-    flexDirection: "row",
-    gap: 5,
-    paddingBottom: 1,
-    marginBottom: 10,
-  },
-  /*forlogo: {},
-  lash: {
-    alignSelf: 'center',
-  },*/
-  dashcont: {
-    flexGrow: 1,
-    width: '100%',
->>>>>>> 95331b237b4181265ce3123a9d8bf83cde61ffe0
     backgroundColor: '#fff',
   },
   menuItem: {
@@ -221,7 +94,6 @@ const Stylers = StyleSheet.create({
     marginVertical: 10,
     marginTop: 20,
   },
-<<<<<<< HEAD
   menuContainer: {
     flexDirection: 'row',
     flexWrap: 'wrap',
@@ -233,13 +105,4 @@ const Stylers = StyleSheet.create({
     width: 60,
     height: 50,
   },
-=======
-icon: {
-    width: 60,
-    height: 50,
-  },
-  /*last: {
-    left: '-20%',
-  },*/
->>>>>>> 95331b237b4181265ce3123a9d8bf83cde61ffe0
 });
